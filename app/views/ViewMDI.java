073ace6747d35a0003b9e6235769251b3c84f79b
unknown <serggio@msn.com>
2019-11-06 22:17:19 -0300

    2019-11-06 : Pr{ctica JMenu

--- app/views/ViewMDI.java
+++ app/views/ViewMDI.java
@@ -1,6 +1,7 @@
 package app.views;
 
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 
 public class ViewMDI extends JFrame
@@ -16,7 +17,7 @@ public class ViewMDI extends JFrame
     private Toolkit toolkit;
     private Dimension screen;
 
-    private PanelMDI PanelMDI;
+    private Menu menu;
 
     public ViewMDI()
     {
@@ -37,23 +38,171 @@ public class ViewMDI extends JFrame
         this.setResizable(false);
         this.setTitle("Aplicacion");
         this.setIconImage(this.appIcon);
+        //this.setLayout(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.PanelMDI = new PanelMDI();
 
-        // Layout.
-        this.add(this.PanelMDI);
+        this.menu = new Menu();
+
+        // Menú.
+        this.setJMenuBar(this.menu);
 
     }
 
-    private class PanelMDI extends JPanel
-    {
+    // Menú.
+    private class Menu extends JMenuBar{
 
-        public void paintComponent(Graphics g){
+        private Archivo      archivo;
+        private Herramientas herramientas;
+        private Exportar     exportar;
+        private Ayuda        ayuda;
 
-            super.paintComponent(g);
-            g.drawRect(10,10,480,180);
+        public Menu(){
+
+            this.setSize(500,24);
+            this.setLocation(0,0);
+            this.setVisible(true);
+            this.archivo      = new Archivo();
+            this.herramientas = new Herramientas();
+            this.exportar     = new Exportar();
+            this.ayuda        = new Ayuda();
+
+            this.add(this.archivo);
+            this.add(this.herramientas);
+            this.add(this.exportar);
+            this.add(this.ayuda);
 
         }
+
+        private class Archivo extends JMenu{
+
+            private Logout logout;
+            private Exit   exit;
+
+            public Archivo(){
+
+                this.setText("Archivo");
+                this.logout = new Logout();
+                this.exit   = new Exit();
+
+                this.add(this.logout);
+                this.add(this.exit);
+
+            }
+
+            private class Logout extends JMenuItem{
+
+                public Logout(){
+
+                    this.setText("Cerrar la sesion.");
+
+                }
+
+            }
+
+            private class Exit extends JMenuItem{
+
+                public Exit(){
+
+                    this.setText("Salir de la aplicación.");
+
+                }
+
+            }
+
+        }
+
+        private class Herramientas extends JMenu{
+
+            public Herramientas(){
+
+                this.setText("Herramientas");
+
+            }
+        }
+
+        private class Exportar extends JMenu{
+
+            private ExportarWord exportarWord;
+            private ExportarWeb  exportarWeb;
+            private ExportarPDF  exportarPDF;
+
+            public Exportar(){
+
+                this.setText("Exportar");
+                this.exportarWord = new ExportarWord();
+                this.exportarWeb  = new ExportarWeb();
+                this.exportarPDF  = new ExportarPDF();
+
+                this.add(this.exportarWord);
+                this.add(this.exportarWeb);
+                this.add(this.exportarPDF);
+
+            }
+
+            private class ExportarWord extends JMenuItem{
+
+                public ExportarWord(){
+
+                    this.setText("Exportar a Word.");
+
+                }
+
+            }
+
+            private class ExportarWeb  extends JMenuItem{
+
+                public ExportarWeb(){
+
+                    this.setText("Exportar a la Web.");
+
+                }
+
+            }
+
+            private class ExportarPDF  extends JMenuItem{
+
+                public ExportarPDF(){
+
+                    this.setText("Exportar a PDF");
+
+                }
+
+            }
+        }
+
+        private class Ayuda extends JMenu{
+
+            private AcercaDe acercaDe;
+            private Manual manual;
+
+            public Ayuda(){
+
+                this.setText("Ayuda...");
+                this.acercaDe = new AcercaDe();
+                this.manual   = new Manual();
+
+                this.add(this.acercaDe);
+                this.add(this.manual);
+            }
+
+            private class AcercaDe extends JMenuItem{
+
+                public AcercaDe(){
+
+                    this.setText("Acerca de...");
+                }
+
+            }
+
+            private class Manual   extends JMenuItem{
+
+                public Manual(){
+
+                    this.setText("Manual de Usuario");
+                }
+            }
+        }
+        
     }
 
 }
